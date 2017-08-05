c90d2e9b849d112f1c13ca14bfa7a8de0bf70641
lys <lyswhut@qq.com>
2017-08-05 20:48:39 +0800

    完善环境，修复bug

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # upload resize image
+
+/**
+ * 图片压缩
+ * @param {String|Array|Object} imgs 图片路径字符串/图片路径数组/file对象
+ * @param {Object} options
+ * @param {Number} [options.width] 图片需要压缩的宽度，高度会跟随调整
+ * @param {Number} [options.quality=0.8] 压缩质量，不压缩为1
+ * @param {String} [options.type] 可选，需要返回的文件类型，如'image/jpeg'、'image/png'等
+ * @param {Function} [options.success(result)] 完成的回调函数，若type有值，则返回blob，否则返回base64
+ * @example
+    resizeImg.getBase64('test.jpg', {
+      width: 200,
+      quality: 0.9,
+      type: image/jpeg',
+      success: function($Blob){
+        formData.append("imgFile", $Blob, "file_" + new Date().getTime() + ".jpg");
+        //...
+      }
+    })
+ */
---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+const gulp = require('gulp'),
+    htmlmin = require('gulp-htmlmin'),//html压缩组件
+    gulpRemoveHtml = require('gulp-remove-html'),//标签清除，参考
+    removeEmptyLines = require('gulp-remove-empty-lines'),//清除空白行，参考
+    babel = require('gulp-babel'),//编译se6
+    concat = require('gulp-concat'),//文件合并
+    uglify = require('gulp-uglify'),//js文件压缩
+    rename = require('gulp-rename'),//文件重命名
+    del = require('del');//删除文件
+
+
+gulp.task('clean:html', function(cb) {
+  del(['dest/**/*.html'], cb);
+});
+gulp.task('clean:js', function(cb) {
+  del(['dest/**/*.js'], cb);
+});
+
+//压缩js
+gulp.task('minifyjs', function(){
+  return gulp.src('src/js/resizeImg.js')
+    .pipe(babel({presets:['es2015']}))//编译se6
+    .pipe(concat('resizeImg.js'))//合并所有js到resizeImg.js
+    .pipe(gulp.dest('dest/js'))//输出resize.js到文件夹
+    .pipe(rename({suffix: '.min'}))//rename压缩后的文件名
+    .pipe(uglify())//压缩
+    .pipe(gulp.dest('dest/js'));//输出
+});
+
+//压缩html
+gulp.task('html',function(){
+  var options = {
+    removeComments: true,//清除HTML注释
+    collapseWhitespace: false,//压缩HTML
+    collapseBooleanAttributes: true,//省略布尔属性的值 <input checked="true"/> ==> <input />
+    removeEmptyAttributes: true,//删除所有空格作属性值 <input id="" /> ==> <input />
+    removeScriptTypeAttributes: true,//删除<script>的type="text/javascript"
+    removeStyleLinkTypeAttributes: true,//删除<style>和<link>的type="text/css"
+    // minifyJS: true,//压缩页面JS
+    // minifyCSS: true//压缩页面CSS
+  };
+  return gulp.src('src/*.html')
+    // .pipe(gulpRemoveHtml())//清除特定标签
+    // .pipe(removeEmptyLines({removeComments: true}))//清除空白行
+    .pipe(htmlmin(options))
+    .pipe(gulp.dest('dest'));
+});
+
+gulp.task('default', function(){
+  gulp.watch('src/js/resizeImg.js', ['clean:js', 'minifyjs']);
+  gulp.watch('src/**/*.html', ['clean:html','html']);
+});
+
